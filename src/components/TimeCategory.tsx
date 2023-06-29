d1c60928908be0c5b7107288c67a7a3be5123fd2
estifanos12 <estif17gtb@gmail.com>
2023-06-29 18:42:47 +0300

    Showing selected time category

--- src/components/TimeCategory.tsx
+++ src/components/TimeCategory.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 type TimeCategoryProps = {
   setTime: React.Dispatch<React.SetStateAction<number>>;
@@ -6,33 +6,38 @@ type TimeCategoryProps = {
 };
 
 const TimeCategory = ({ setTime, updateWords }: TimeCategoryProps) => {
+  const [selected, setSelected] = useState(1);
+
   return (
     <div className='flex justify-center items-center gap-3'>
       <h2 className='font-mono text-xl'>Time Category</h2>
       <div className='flex gap-4 bg-secondary rounded-lg'>
         <span
-          className='category'
+          className={`category ${selected === 1 ? 'underline' : ''}`}
           onClick={() => {
             setTime(15000);
             updateWords();
+            setSelected(1);
           }}
         >
           15
         </span>
         <span
-          className='category'
+          className={`category ${selected === 2 ? 'underline' : ''}`}
           onClick={() => {
             setTime(30000);
             updateWords();
+            setSelected(2);
           }}
         >
           30
         </span>
         <span
-          className='category'
+          className={`category ${selected === 3 ? 'underline' : ''}`}
           onClick={() => {
             setTime(60000);
             updateWords();
+            setSelected(3);
           }}
         >
           60
