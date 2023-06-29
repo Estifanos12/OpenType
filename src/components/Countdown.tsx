599d767296d33fa1701170dd01ddbc52a27e84e8
estifanos12 <estif17gtb@gmail.com>
2023-06-29 17:09:27 +0300

    Creating countdown components

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+
+type CountdownProps = {
+  countdown: number;
+  reset: () => void;
+};
+
+const Countdown = ({ countdown, reset }: CountdownProps) => {
+  useEffect(() => {
+    reset();
+  }, [reset]);
+
+  const formatedCountdown = {
+    minutes: new Date(countdown).getUTCMinutes(),
+    seconds: new Date(countdown).getUTCSeconds(),
+  };
+
+  return (
+    <div>
+      <div className='flex justify-end py-5'>
+        <span className='text-lg font-mono text-right'>
+          {formatedCountdown.minutes < 10
+            ? `0${formatedCountdown.minutes}`
+            : formatedCountdown.minutes}
+          :
+          {formatedCountdown.seconds < 10
+            ? `0${formatedCountdown.seconds}`
+            : formatedCountdown.seconds}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
