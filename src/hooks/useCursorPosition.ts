fc1dd8f28c94e79e686b503b5f37ab7107b621b8
estifanos12 <estif17gtb@gmail.com>
2023-07-02 13:28:54 +0300

    Adding reset function

--- src/hooks/useCursorPosition.ts
+++ src/hooks/useCursorPosition.ts
@@ -3,10 +3,12 @@ import { useState } from 'react';
 export const useCursorPosition = () => {
   const [cursorPosition, setCursorPosition] = useState(0);
 
+  const resetCursorPointer = () => setCursorPosition(0);
+
   const updateCursorPosition = (opt: 'increase' | 'decrease') => {
     if (opt === 'increase') setCursorPosition((cursor) => cursor + 1);
-    setCursorPosition((cursor) => cursor - 1);
+    else setCursorPosition((cursor) => cursor - 1);
   };
 
-  return { cursorPosition, updateCursorPosition };
+  return { cursorPosition, updateCursorPosition, resetCursorPointer };
 };
