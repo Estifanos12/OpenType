8da9d4942bb5e65dc13325b0f7aac82f63ae0171
estifanos12 <estif17gtb@gmail.com>
2023-07-03 20:06:14 +0300

    Adding stats function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,27 @@ export const isAllowedCode = (code: string): boolean => {
 export const generateWord = (n: number): string => {
   return faker.word.words(n);
 };
+
+export const calculateAccuracy = (expectedWord: string, typedWord: string) => {
+  console.log(expectedWord, typedWord);
+  const words = expectedWord.split(' ');
+  const typedWords = typedWord.split(' ');
+  let correctWords = 0;
+
+  typedWords.forEach((word, index) => {
+    if (word === words[index]) {
+      correctWords++;
+    }
+  });
+  return (correctWords / typedWords.length) * 100;
+};
+
+export const calculateWPM = (typedWord: string, time: number) => {
+  const words = typedWord.split(' ');
+  const minutes = time / 60000;
+  return words.length / minutes;
+};
+
+export const calculateErrorPercentage = (accuracy: number) => {
+  return 100 - accuracy;
+};
