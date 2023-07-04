1eea8617954acf45cf0ba129845e4c9330dddc41
estifanos12 <estif17gtb@gmail.com>
2023-07-04 16:34:11 +0300

    Adding about modal handler

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -2,14 +2,17 @@ import { useState, useCallback } from 'react';
 
 export const useModal = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [aboutModal, setAboutModal] = useState(false);
 
-  const openModal = useCallback(() => {
-    setModalIsOpen(true);
+  const openModal = useCallback((type: string) => {
+    if (type === 'result') setModalIsOpen(true);
+    else setAboutModal(true);
   }, []);
 
-  const closeModal = useCallback(() => {
-    setModalIsOpen(false);
+  const closeModal = useCallback((type: string) => {
+    if (type === 'result') setModalIsOpen(false);
+    else setAboutModal(false);
   }, []);
 
-  return { modalIsOpen, openModal, closeModal };
+  return { modalIsOpen, aboutModal, openModal, closeModal };
 };
