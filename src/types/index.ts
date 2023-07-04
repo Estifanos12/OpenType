4863378961205eb8f5862b536024cf9f7c105665
estifanos12 <estif17gtb@gmail.com>
2023-07-04 16:34:37 +0300

    Adding history interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,11 @@
 export interface Results {
   accuracy: number;
+  cpm: number;
   wpm: number;
   error: number;
+}
+
+export interface HistoryType {
+  wordHistory: string;
+  typedHistory: string;
 }
