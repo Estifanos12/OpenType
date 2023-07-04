1773a331b885edb012de8e27b5ac69aafda21d5e
estifanos12 <estif17gtb@gmail.com>
2023-07-04 22:40:51 +0300

    Add space to generated words

--- src/hooks/useWord.ts
+++ src/hooks/useWord.ts
@@ -3,11 +3,11 @@ import { useState, useCallback } from 'react';
 import { generateWord } from '../utils';
 
 export const useWord = (numberOfWords: number) => {
-  const [word, setWord] = useState<string>(generateWord(numberOfWords));
+  const [word, setWord] = useState<string>(generateWord(numberOfWords) + ' ');
   const [totalWord, setTotalWord] = useState<string>(word);
 
   const appendWord = useCallback((word: string) => {
-    setTotalWord((prev) => prev + ' ' + word);
+    setTotalWord((prev) => prev + word);
   }, []);
   const eraseWord = useCallback((word: string) => {
     setTotalWord(word);
@@ -16,7 +16,7 @@ export const useWord = (numberOfWords: number) => {
   const updateWord = useCallback(
     (erase = false) => {
       setWord(() => {
-        const genWord = generateWord(numberOfWords);
+        const genWord = generateWord(numberOfWords) + ' ';
         if (erase) eraseWord(genWord);
         else appendWord(genWord);
         return genWord;
