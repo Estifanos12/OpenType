7da5a751c3357c618d72fefd73a5576dee0499bd
estifanos12 <estif17gtb@gmail.com>
2023-07-05 01:01:51 +0300

    Detect user agent device

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,6 +9,27 @@ export const isAllowedCode = (code: string): boolean => {
   );
 };
 
+export const isMobile = () => {
+  const userAgent = navigator.userAgent;
+
+  const mobileUserAgents = [
+    'Android',
+    'iPhone',
+    'iPad',
+    'iPod',
+    'BlackBerry',
+    'Windows Phone',
+  ];
+
+  for (let i = 0; i < mobileUserAgents.length; i++) {
+    if (userAgent.indexOf(mobileUserAgents[i]) !== -1) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 export const generateWord = (n: number): string => {
   return faker.word.words(n);
 };
