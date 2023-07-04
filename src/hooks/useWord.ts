d7a78f3eaed2f30d1d563ba5574a36949198700c
estifanos12 <estif17gtb@gmail.com>
2023-07-04 16:34:57 +0300

    Track total expected word

--- src/hooks/useWord.ts
+++ src/hooks/useWord.ts
@@ -4,10 +4,26 @@ import { generateWord } from '../utils';
 
 export const useWord = (numberOfWords: number) => {
   const [word, setWord] = useState<string>(generateWord(numberOfWords));
+  const [totalWord, setTotalWord] = useState<string>(word);
 
-  const updateWord = useCallback(() => {
-    setWord(generateWord(numberOfWords));
-  }, [numberOfWords]);
+  const appendWord = useCallback((word: string) => {
+    setTotalWord((prev) => prev + ' ' + word);
+  }, []);
+  const eraseWord = useCallback((word: string) => {
+    setTotalWord(word);
+  }, []);
 
-  return { word, updateWord };
+  const updateWord = useCallback(
+    (erase = false) => {
+      setWord(() => {
+        const genWord = generateWord(numberOfWords);
+        if (erase) eraseWord(genWord);
+        else appendWord(genWord);
+        return genWord;
+      });
+    },
+    [numberOfWords, appendWord, eraseWord]
+  );
+
+  return { word, totalWord, setTotalWord, updateWord, appendWord };
 };
