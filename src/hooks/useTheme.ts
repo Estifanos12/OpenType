b08911ba5ce3cede36295f4bdf2d03ff7c31dbb6
estifanos12 <estif17gtb@gmail.com>
2023-07-07 00:46:28 +0300

    add: Add theme functionality

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,30 @@
+import { useState, useCallback, useContext } from 'react';
+
+import { useLocalStorage } from './useLocalStorage';
+
+import { ThemeContext } from '../context/ThemeContext';
+import { theme } from '../utils';
+import { Theme } from '../types';
+
+export const useTheme = () => {
+  const { getLocalStorageValue, setLocalStorageValue } = useLocalStorage();
+
+  const [systemTheme, setSystemTheme] = useState<Theme>(() => {
+    const localTheme = getLocalStorageValue('theme');
+    return localTheme ? localTheme : theme.blueDolphin;
+  });
+
+  const setTheme = useCallback(
+    (value: Theme) => {
+      setSystemTheme(value);
+      setLocalStorageValue('theme', value);
+    },
+    [setSystemTheme, setLocalStorageValue]
+  );
+
+  return { systemTheme, setTheme };
+};
+
+export const useThemeContext = () => {
+  return useContext(ThemeContext);
+};
