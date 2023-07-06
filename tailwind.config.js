a5f8576156391151bb6473f263a02690a7cfa07d
estifanos12 <estif17gtb@gmail.com>
2023-07-07 00:43:00 +0300

    remove: Remove unnecessary property extending

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundColor: {
-        primary: "#003950",
-        secondary: "#014961",
-        tootip: "#1C2B3A",
-      },
-      outlineColor: {
-        focused: "#FFCEFB",
-      },
-      textColor: {
-        accent: "#6DEAFF",
-        secondaryAccent: "#FFCEFB",
-        type: "#BCA7CE",
-      },
       keyframes: {
         appear: {
           "0%": { opacity: 0 },
