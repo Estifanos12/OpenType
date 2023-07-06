3a437f563fd083fce4a77a1b116e82e5ccd37b5b
estifanos12 <estif17gtb@gmail.com>
2023-07-07 00:47:11 +0300

    add: Add more themes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -28,7 +28,6 @@ export const isMobile = () => {
       return true;
     }
   }
-
   return false;
 };
 
@@ -71,4 +70,79 @@ export const calculateWPM = (
 
 export const calculateErrorPercentage = (accuracy: number) => {
   return 100 - accuracy;
+};
+
+export const theme = {
+  blueDolphin: {
+    name: 'Blue Dolphin',
+    background: {
+      primary: '#003950',
+      secondary: '#014961',
+    },
+    text: {
+      primary: '#6DEAFF',
+      secondary: '#FFCEFB',
+      title: '#6DEAFF',
+    },
+  },
+  aurora: {
+    name: 'Aurora',
+    background: {
+      primary: '#011926',
+      secondary: '#000C13',
+    },
+    text: {
+      primary: '#235A68',
+      secondary: '#00E980',
+      title: '#00E980',
+    },
+  },
+  paper: {
+    name: 'Paper',
+    background: {
+      primary: '#EEEEEE',
+      secondary: '#DDDDDD',
+    },
+    text: {
+      primary: '#B4B4B4',
+      secondary: '#444444',
+      title: '#444444',
+    },
+  },
+  cyberspace: {
+    name: 'Cyberspace',
+    background: {
+      primary: '#181C18',
+      secondary: '#131613',
+    },
+    text: {
+      primary: '#9578D3',
+      secondary: '#04AF6A',
+      title: '#9578D3',
+    },
+  },
+  cheesecake: {
+    name: 'Cheesecake',
+    background: {
+      primary: '#FDF0D5',
+      secondary: '#F3E2BF',
+    },
+    text: {
+      primary: '#E14C94',
+      secondary: '#3A3335',
+      title: '#E14C94',
+    },
+  },
+  bouquet: {
+    name: 'Bouquet',
+    background: {
+      primary: '#173F35',
+      secondary: '#1F4E43',
+    },
+    text: {
+      primary: '#408E7B',
+      secondary: '#DBE0D2',
+      title: '#DBE0D2',
+    },
+  },
 };
